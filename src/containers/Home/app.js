18acc76eac7c357c9647dbda64ffa628bcc764f5
ZhaoXiaodong <zhaoxd@zenmen.com>
2018-08-14 17:15:41 +0800

    feat: add Loading to Home

--- src/containers/Home/app.js
+++ src/containers/Home/app.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import Todo from 'components/Todo';
+import Loading from 'components/Loading';
 import style from './home.scss';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false }), 1000);
   }
   
   render() {
     return (
-      <div className={style.container}>
-        <h1>Home</h1>
-        <Todo />
+      <div className="container">
+        <h1 className={style.title}>Home</h1>
+        <Loading visible={this.state.loading} />
       </div>
     );
   }
