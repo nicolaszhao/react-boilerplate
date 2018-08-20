60f768ea6f476ec7f48e1968ea9c7472047c04ae
ZhaoXiaodong <zhaoxd@zenmen.com>
2018-08-20 12:05:54 +0800

    chore: update config.zero

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const zeroConfig = require('webpack-config-zero');
+const path = require('path');
 
 module.exports = zeroConfig({
-  spa: false
+  spa: false,
+  include: {
+    js: [
+
+      // TODO: 该模块包需要用 rollup 构建成 es 模块
+      path.resolve(__dirname, 'node_modules', 'tote-box')
+      
+    ]
+  }
 });
