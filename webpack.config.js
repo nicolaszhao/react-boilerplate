d4a66d06b4c3a56a2eaafc7cf6a2f5d1a86fd237
ZhaoXiaodong <zhaoxd@zenmen.com>
2018-08-28 10:13:40 +0800

    chore: add babel-plugin-lodash

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,5 @@
 const zeroConfig = require('webpack-config-zero');
-const path = require('path');
 
 module.exports = zeroConfig({
-  spa: false,
-  include: {
-    js: [
-
-      // TODO: 该模块包需要用 rollup 构建成 es 模块
-      path.resolve(__dirname, 'node_modules', 'tote-box')
-      
-    ]
-  }
+  spa: false
 });
