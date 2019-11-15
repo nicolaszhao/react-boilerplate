503dfc1d559dfefd59b1e0c5f47735c2bc9441e4
Nicolas <zhaoxd@zenmen.com>
2019-11-15 14:38:55 +0800

    chore: 更新项目配置文件

---
+++ hammal.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pages: {
+    index: 'src/pages/Home/index.js',
+    about: 'src/pages/About/index.js',
+  },
+};
--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-const zeroConfig = require('webpack-config-zero');
-
-module.exports = zeroConfig({
-  spa: false
-});
